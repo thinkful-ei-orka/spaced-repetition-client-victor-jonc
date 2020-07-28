5c0b3794cef86a13b6146c40ac30af377816d012
Victor Jarvis <vctrjrvs@gmail.com>
2020-07-28 14:28:08 -0400

    fetch call change

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,27 +6,36 @@ import config from '../../config';
 
 export default class Dashboard extends Component {
   state = {
-    error: null,
-    isLoading: true,
+    error: null
   };
+
   static contextType = UserContext;
 
   getData = () => {
-    return fetch(`${config.API_ENDPOINT}/language`, {
+    console.log('line 15')
+     fetch(`${config.API_ENDPOINT}/language`, {
       method: 'GET',
       headers: {
         authorization: `bearer ${TokenService.getAuthToken()}`,
-      },
+      }
     })
-      .then((res) => {
-        const language = res.language.json();
-        console.log(language);
-        //    this.context.setWords(words);
-        this.context.setLanguage(language);
-      })
-      .then(() => this.context.setIsLoading())
+    .then(res => res.json())
+    .then(res => {
+      this.context.setWords(res.words)
+      this.context.setLanguage(res.language)
+      this.context.setIsLoading();
+      console.log(res.language);
+      return;
+    })
+      // .then((res) => {
+      //   const language = res.language.json();
+      //   console.log(language);
+      //   //    this.context.setWords(words);
+      //   this.context.setLanguage(language);
+      // })
+      // .then(() => this.context.setIsLoading())
 
-      .catch((error) => this.setState({ error: error }));
+      .catch(error => this.setState({ error: error }));
   };
 
   componentDidMount() {
@@ -34,7 +43,7 @@ export default class Dashboard extends Component {
   }
 
   render() {
-    if (this.context.isLoading === true) {
+    if (this.context.isLoading) {
       return <div>Loading...</div>;
     }
     return (
